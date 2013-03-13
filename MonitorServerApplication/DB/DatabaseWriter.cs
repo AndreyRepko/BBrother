619b3354457a2b0ff9d7a0e6cf6809ba8102474c
Andrey <arepko@gmail.com>
2013-03-13 17:56:12 +0200

    Writers, small add

--- MonitorServerApplication/DB/DatabaseWriter.cs
+++ MonitorServerApplication/DB/DatabaseWriter.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace MonitorServerApplication.ServerThreading
 {
     class DatabaseWriter
     {
+        public NpgsqlConnection PgConnect;
+        public bool IsConnected = false;
+
+        public DatabaseWriter(NpgsqlConnectionStringBuilder connectionParams)
+        {
+            PgConnect = new NpgsqlConnection(connectionParams.ConnectionString);
+        }
+
+        protected void CheckConnected()
+        {
+           
+            /*if (PgConnect.Te)
+        {
+        }*/
+
+        }
+
     }
 }
